b0dd5e25f5edc38622f6b51388825dc733f42704
Manu BuddHa <ferano915@gmail.com>
2025-06-11 13:04:47 +0530

    Search Results Working

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ class voice:
                     audio = self.recognizer.listen(source)
                     command = self.recognizer.recognize_google(audio).lower()
                     print(f"Command received: {command}")
+                    if "search" in command:
+                        driver = webdriver.Chrome()
+                        driver.get(f"https://www.google.com/search?channel=trow2&client=firefox-b-d&q={command.split('search ')[-1]}")
             except sr.UnknownValueError:
                 pass
+            except KeyboardInterrupt:
+                print("Exiting...")
+                break
 listener=voice()
